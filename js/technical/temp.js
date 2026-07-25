41f118ed71bdf0642e3949d0e19fff89e21a8b5a
loader3229 <1010903229@qq.com>
2026-07-25 12:32:51 +0806

    add grantMilestone and grantAchievement

--- js/technical/temp.js
+++ js/technical/temp.js
@@ -107,7 +107,7 @@ function updateTemp() {
 		tmp[layer].notify = shouldNotify(layer)
 		tmp[layer].prestigeNotify = prestigeNotify(layer)
 		if (tmp[layer].passiveGeneration === true) tmp[layer].passiveGeneration = 1 // new Decimal(true) = decimalZero
-
+		tmp[layer].passiveGeneration = new Decimal(tmp[layer].passiveGeneration)
 	}
 
 	tmp.pointGen = getPointGen()
